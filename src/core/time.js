2d8e4ff63f465e1ebf9ac05222539d3bc1c3edc7
Supersonic Seven <b.rockholz@icloud.com>
2025-10-28 18:28:40 -0400

    Update time.js
    This gonna error huh

--- src/core/time.js
+++ src/core/time.js
@@ -80,7 +80,7 @@ export const Time = {
    * @returns {TimeSpan}
    */
   get realTimeDoomed() {
-    return this.fromMilliseconds(() => new Decimal(player.records.realTimeDoomed));
+    return this.fromMilliseconds(() => new Decimal(player.records.realTimeDoomed).toNumber());
   },
   /**
    * @param {TimeSpan} timespan
@@ -93,7 +93,7 @@ export const Time = {
    * @returns {TimeSpan}
    */
   get realTimePlayed() {
-    return this.fromMilliseconds(() => new Decimal(player.records.realTimePlayed));
+    return this.fromMilliseconds(() => new Decimal(player.records.realTimePlayed).toNumber());
   },
   /**
    * @param {TimeSpan} timespan
@@ -119,7 +119,7 @@ export const Time = {
    * @returns {TimeSpan}
    */
   get thisInfinityRealTime() {
-    return this.fromMilliseconds(() => new Decimal(player.records.thisInfinity.realTime));
+    return this.fromMilliseconds(() => new Decimal(player.records.thisInfinity.realTime).toNumber());
   },
   /**
    * @param {TimeSpan} timespan
@@ -145,7 +145,7 @@ export const Time = {
    * @returns {TimeSpan}
    */
   get bestInfinityRealTime() {
-    return this.fromMilliseconds(() => new Decimal(player.records.bestInfinity.realTime));
+    return this.fromMilliseconds(() => new Decimal(player.records.bestInfinity.realTime).toNumber());
   },
   /**
    * @param {TimeSpan} timespan
@@ -171,7 +171,7 @@ export const Time = {
    * @returns {TimeSpan}
    */
   get thisEternityRealTime() {
-    return this.fromMilliseconds(() => new Decimal(player.records.thisEternity.realTime));
+    return this.fromMilliseconds(() => new Decimal(player.records.thisEternity.realTime).toNumber());
   },
   /**
    * @param {TimeSpan} timespan
@@ -197,7 +197,7 @@ export const Time = {
    * @returns {TimeSpan}
    */
   get bestEternityRealTime() {
-    return this.fromMilliseconds(() => new Decimal(player.records.bestEternity.realTime));
+    return this.fromMilliseconds(() => new Decimal(player.records.bestEternity.realTime).toNumber());
   },
   /**
    * @param {TimeSpan} timespan
@@ -224,7 +224,7 @@ export const Time = {
    * @returns {TimeSpan}
    */
   get thisRealityRealTime() {
-    return this.fromMilliseconds(() => new Decimal(player.records.thisReality.realTime));
+    return this.fromMilliseconds(() => new Decimal(player.records.thisReality.realTime).toNumber());
   },
   /**
    * @param {TimeSpan} timespan
@@ -250,7 +250,7 @@ export const Time = {
    * @returns {TimeSpan}
    */
   get bestRealityRealTime() {
-    return this.fromMilliseconds(() => new Decimal(player.records.bestReality.realTime));
+    return this.fromMilliseconds(() => new Decimal(player.records.bestReality.realTime).toNumber());
   },
   /**
    * @param {TimeSpan} timespan
