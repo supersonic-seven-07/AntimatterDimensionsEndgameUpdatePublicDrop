60dfbec75160ea7f5255b79daa63e81a2a0be0a1
Supersonic Seven <b.rockholz@icloud.com>
2025-11-05 16:26:36 -0500

    Update pelle.js
    Fixing a smol bug

--- src/core/celestials/pelle/pelle.js
+++ src/core/celestials/pelle/pelle.js
@@ -381,7 +381,7 @@ export const Pelle = {
 
   // Calculations assume this is in units of proportion per second (eg. 0.03 is 3% drain per second)
   get riftDrainPercent() {
-    const extraDrain = Math.max(0.45, player.endgames * 0.05);
+    const extraDrain = Math.min(0.45, player.endgames * 0.05);
     if (EndgameMilestone.riftFill.isReached) return 0.05 + extraDrain;
     return 0.05;
   },
