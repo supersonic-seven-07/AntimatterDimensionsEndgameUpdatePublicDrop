39c2291c813cc82e9f6feffb5f8b644477b5cc99
Supersonic Seven <b.rockholz@icloud.com>
2025-11-07 13:31:43 -0500

    Update ClassicAntimatterDimensionRow.vue
    Ayyyyyyyyyy

--- src/components/tabs/antimatter-dimensions/ClassicAntimatterDimensionRow.vue
+++ src/components/tabs/antimatter-dimensions/ClassicAntimatterDimensionRow.vue
@@ -93,7 +93,7 @@ export default {
     update() {
       const tier = this.tier;
       if (tier === 8 && this.isDoomed) this.formattedAmount = formatInt(this.amount);
-      if (tier > DimBoost.maxDimensionsUnlockable) return;
+      if (tier > DimBoost.maxDimensionsUnlockable.toNumber()) return;
       const dimension = AntimatterDimension(tier);
       this.isUnlocked = dimension.isAvailableForPurchase;
       this.isCapped = tier === 8 && Enslaved.isRunning && dimension.bought >= 1;
@@ -112,7 +112,7 @@ export default {
       this.isContinuumActive = Laitela.continuumActive;
       if (this.isContinuumActive) this.continuumValue = dimension.continuumValue;
       this.isShown =
-        (DimBoost.totalBoosts > 0 && DimBoost.totalBoosts + 3 >= tier) || PlayerProgress.infinityUnlocked();
+        (DimBoost.totalBoosts.gt(0) && DimBoost.totalBoosts.plus(3).toNumber() >= tier) || PlayerProgress.infinityUnlocked();
       this.isCostsAD = NormalChallenge(6).isRunning && tier > 2 && !this.isContinuumActive;
       this.hasTutorial = (tier === 1 && Tutorial.isActive(TUTORIAL_STATE.DIM1)) ||
         (tier === 2 && Tutorial.isActive(TUTORIAL_STATE.DIM2));
