e28829a2b9c7d1c96160285e6da43bd369dcb082
Supersonic Seven <b.rockholz@icloud.com>
2026-01-09 11:31:29 -0500

    Update laitela.js

--- src/core/celestials/laitela/laitela.js
+++ src/core/celestials/laitela/laitela.js
@@ -33,6 +33,7 @@ export const Laitela = {
     return Laitela.maxAllowedDimension === 0;
   },
   get continuumUnlocked() {
+    if (EndgameMilestone.celestialEarlyUnlock.isReached) return true;
     return ImaginaryUpgrade(15).isBought;
   },
   get continuumActive() {
@@ -46,26 +47,51 @@ export const Laitela = {
     }
   },
   get matterExtraPurchaseFactor() {
-    return (1 + 0.5 * Math.pow(Decimal.pLog10(Currency.darkMatter.max) / 50, 0.4) *
-      (1 + SingularityMilestone.continuumMult.effectOrDefault(0)));
+    if (Pelle.isDoomed && !PelleDestructionUpgrade.continuumBuff.isBought) return 1;
+    return (Decimal.pow(new Decimal(Decimal.log10(Currency.darkMatter.max)).div(50), 0.4).times(0.5).add(1).times(
+      SingularityMilestone.continuumMult.effectOrDefault(new Decimal(0)).add(1))).toNumber();
+  },
+  get hadronizes() {
+    return this.celestial.hadronizes;
   },
   get realityReward() {
-    return Math.clampMin(Math.pow(100, this.difficultyTier) *
-      Math.pow(360 / player.celestials.laitela.fastestCompletion, 2), 1);
+    const fullLaitelaCompletion = Decimal.clampMin(Decimal.pow(100, 8).times(Decimal.pow(new Decimal(360).div(300), 2)), 1);
+    const currentLaitelaProgress = Decimal.clampMin(Decimal.pow(100, this.difficultyTier).times(
+      Decimal.pow(new Decimal(360).div(player.celestials.laitela.fastestCompletion), 2)), 1);
+    return Decimal.pow(currentLaitelaProgress, this.hadronizes + 1).times(Decimal.pow(
+      fullLaitelaCompletion, (this.hadronizes * (this.hadronizes + 1)) / 2));
+  },
+  get realityRewardDE() {
+    const fullDestabilization = this.isFullyDestabilized ? Math.pow(8, this.hadronizes + 1) : 1;
+    return fullDestabilization * Math.pow(8, (this.hadronizes * (this.hadronizes + 1)) / 2);
   },
   // Note that entropy goes from 0 to 1, with 1 being completion
   get entropyGainPerSecond() {
-    return Math.clamp(Math.pow(Currency.antimatter.value.add(1).log10() / 1e11, 2), 0, 100) / 200;
+    const maxSpeed = ExpansionPack.laitelaPack.isBought ? 1000 : 100;
+    const hadronizeBump = this.hadronizes > 0 ? 1e12 : 1;
+    const hadronizeAntimatter = Decimal.pow(1000, this.hadronizes).times(hadronizeBump).times(1e11);
+    return Decimal.clamp(Decimal.pow(new Decimal(Currency.antimatter.value.add(1).log10()).div(hadronizeAntimatter), 2), 0, maxSpeed).div(200);
   },
   get darkMatterMultGain() {
-    return Decimal.pow(Currency.darkMatter.value.dividedBy(this.annihilationDMRequirement)
-      .plus(1).log10(), 1.5).toNumber() * ImaginaryUpgrade(21).effectOrDefault(1);
+    const extraPow = ExpansionPack.laitelaPack.isBought
+      ? Decimal.pow((Math.log10(Decimal.log10(Currency.darkMatter.value) + 1) + 1) / 2, 2).add(1) : 1;
+    return Decimal.pow(Decimal.pow(Currency.darkMatter.value.dividedBy(this.annihilationDMRequirement)
+      .plus(1).log10(), 1.5).times(ImaginaryUpgrade(21).effectOrDefault(1)), extraPow);
   },
   get darkMatterMult() {
     return this.celestial.darkMatterMult;
   },
   get darkMatterMultRatio() {
-    return (this.celestial.darkMatterMult + this.darkMatterMultGain) / this.celestial.darkMatterMult;
+    return (this.celestial.darkMatterMult.add(this.darkMatterMultGain)).div(this.celestial.darkMatterMult);
+  },
+  get darkMatterCap() {
+    let baseCap = Decimal.NUMBER_MAX_VALUE;
+    if (ImaginaryUpgrade(26).isBought) baseCap = DC.E1000;
+    if (ImaginaryUpgrade(27).isBought) baseCap = DC.E4000;
+    if (ImaginaryUpgrade(28).isBought) baseCap = DC.E20000;
+    if (ImaginaryUpgrade(29).isBought) baseCap = DC.E100000;
+    const realityReward = ExpansionPack.laitelaPack.isBought ? this.realityReward : 1;
+    return baseCap.times(EndgameUpgrade(4).effectOrDefault(1)).times(realityReward);
   },
   get annihilationUnlocked() {
     return ImaginaryUpgrade(19).isBought;
@@ -78,7 +104,7 @@ export const Laitela = {
   },
   annihilate(force) {
     if (!force && !this.canAnnihilate) return false;
-    this.celestial.darkMatterMult += this.darkMatterMultGain;
+    this.celestial.darkMatterMult = this.celestial.darkMatterMult.add(this.darkMatterMultGain);
     DarkMatterDimensions.reset();
     Laitela.quotes.annihilation.show();
     Achievement(176).unlock();
@@ -92,34 +118,42 @@ export const Laitela = {
     const upgradeInfo = unlockedDimensions
       .map(d => [
         [d.rawIntervalCost, d.intervalCostIncrease, d.maxIntervalPurchases, x => d.buyManyInterval(x)],
-        [d.rawPowerDMCost, d.powerDMCostIncrease, Infinity, x => d.buyManyPowerDM(x)],
-        [d.rawPowerDECost, d.powerDECostIncrease, Infinity, x => d.buyManyPowerDE(x)]])
+        [d.rawPowerDMCost, d.powerDMCostIncrease, Decimal.MAX_VALUE, x => d.buyManyPowerDM(x)],
+        [d.rawPowerDECost, d.powerDECostIncrease, Decimal.MAX_VALUE, x => d.buyManyPowerDE(x)]])
       .flat(1);
     const buy = function(upgrade, purchases) {
       upgrade[3](purchases);
       upgrade[0] = upgrade[0].times(Decimal.pow(upgrade[1], purchases));
-      upgrade[2] -= purchases;
+      upgrade[2] = upgrade[2].sub(purchases);
     };
     // Buy everything costing less than 0.02 of initial matter.
     const darkMatter = Currency.darkMatter.value;
     for (const upgrade of upgradeInfo) {
-      const purchases = Math.clamp(Math.floor(darkMatter.times(0.02).div(upgrade[0]).log(upgrade[1])), 0, upgrade[2]);
+      const purchases = Decimal.clamp(Decimal.floor(darkMatter.times(0.02).div(upgrade[0]).log(upgrade[1].toNumber())), 0, upgrade[2]);
       buy(upgrade, purchases);
     }
-    while (upgradeInfo.some(upgrade => upgrade[0].lte(darkMatter) && upgrade[2] > 0)) {
-      const cheapestUpgrade = upgradeInfo.filter(upgrade => upgrade[2] > 0).sort((a, b) => a[0].minus(b[0]).sign())[0];
-      buy(cheapestUpgrade, 1);
+    while (upgradeInfo.some(upgrade => upgrade[0].lte(darkMatter) && upgrade[2].gt(0))) {
+      const cheapestUpgrade = upgradeInfo.filter(upgrade => upgrade[2].gt(0)).sort((a, b) => new Decimal(Decimal.compare(a[0], b[0])).sign())[0];
+      buy(cheapestUpgrade, DC.D1);
     }
+  },
+  hadronize() {
+    if (!Laitela.isFullyDestabilized) return false;
+    this.celestial.fastestCompletion = 3600;
+    this.celestial.difficultyTier = 0;
+    this.celestial.hadronizes += 1;
+    return true;
   },
   reset() {
     this.annihilate(true);
-    this.celestial.darkMatterMult = 1;
+    this.celestial.darkMatterMult = DC.D1;
     Currency.darkMatter.max = DC.D1;
     Currency.darkMatter.reset();
+    Currency.unnerfedDarkMatter.reset();
     Currency.singularities.reset();
     this.celestial.fastestCompletion = 3600;
     this.celestial.difficultyTier = 0;
-    this.celestial.singularityCapIncreases = 0;
+    this.celestial.singularityCapIncreases = DC.D0;
   },
   quotes: Quotes.laitela,
   symbol: "ᛝ"
