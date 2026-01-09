72b0662a2f49341972b31595c9d44984102fdc3b
Supersonic Seven <b.rockholz@icloud.com>
2026-01-09 09:53:12 -0500

    Update EndgameTab.vue

--- src/components/tabs/endgame/EndgameTab.vue
+++ src/components/tabs/endgame/EndgameTab.vue
@@ -1,26 +1,42 @@
+<script>
+import ResetEndgameButton from "./ResetEndgameButton";
+export default {
+  name: "EndgameTab",
+  components: {
+    ResetEndgameButton
+  }
+};
+</script>
+
 <template>
-  <div class="endgame-text">
-    <br>
-    <div>
-    <b>
-      Welcome to Endgame
-    </b>
+  <div>
+    <div class="endgame-text">
+      <br>
+      <div>
+      <b>
+        Welcome to Endgame
+      </b>
+      </div>
+      <br>
+      <div>
+        Feel free to look around! You've just unlocked a new Prestige Layer!
+      </div>
+      <br>
+      <div>
+        And don't worry about Pelle. I'm sure he's dead... right?
+      </div>
+      <br>
+      <div>
+        Eh, whatever. We've got more important things to worry about.
+      </div>
+      <br>
+      <div>
+        Why don't you go ahead and get started?
+      </div>
     </div>
     <br>
-    <div>
-      Feel free to look around! You've just unlocked a new Prestige Layer!
-    </div>
-    <br>
-    <div>
-      And don't worry about Pelle. I'm sure he's dead... right?
-    </div>
-    <br>
-    <div>
-      Eh, whatever. We've got more important things to worry about.
-    </div>
-    <br>
-    <div>
-      Why don't you go ahead and get started?
+    <div class="l-endgame-button-column">
+      <ResetEndgameButton/>
     </div>
   </div>
 </template>
