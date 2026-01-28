d9b3e745e52385820fa6467609bd81ece218c36f
Supersonic Seven <b.rockholz@icloud.com>
2026-01-28 10:22:58 -0500

    Update big-crunch.js
    Packer

--- src/core/big-crunch.js
+++ src/core/big-crunch.js
@@ -183,7 +183,7 @@ export function preProductionGenerateIP(diff) {
     let gainedPerGen = player.records.bestInfinity.time.gte(999999999999) ? DC.D0 : InfinityUpgrade.ipGen.effectValue;
     if (Laitela.isRunning && gainedPerGen.gt(1)) gainedPerGen = dilatedValueOf(gainedPerGen);
     const gainedThisTick = new Decimal(genCount).times(gainedPerGen);
-    Currency.infinityPoints.add(gainedThisTick);
+    if (isFinite(gainedThisTick)) Currency.infinityPoints.add(gainedThisTick);
   }
   Currency.infinityPoints.add(BreakInfinityUpgrade.ipGen.effectOrDefault(DC.D0).times(diff).div(60000));
 }
