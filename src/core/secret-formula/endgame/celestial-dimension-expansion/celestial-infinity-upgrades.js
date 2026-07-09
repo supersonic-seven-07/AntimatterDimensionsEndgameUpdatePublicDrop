1fda00db705e1d7ae77815365ffdb1aa78b37228
Supersonic Seven <b.rockholz@icloud.com>
2026-07-09 18:49:15 -0400

    Adjust effect calculation for alphaDecayStartBoost

--- src/core/secret-formula/endgame/celestial-dimension-expansion/celestial-infinity-upgrades.js
+++ src/core/secret-formula/endgame/celestial-dimension-expansion/celestial-infinity-upgrades.js
@@ -16,8 +16,8 @@ export const celestialInfinityUpgrades = {
     id: "alphaDecayStartBoost",
     cost: 5,
     description: () => `Gain a very small boost to Alpha Decay starting time based on Celestial Infinities`,
-    effect: () => Decimal.pow(player.endgame.celDimExpansion.celestialInfinities, 0.5).div(300).min(1).add(
-      DC.D4.times(DC.D1.sub(Decimal.pow(0.8, player.endgame.celDimExpansion.celestialInfinities.add(10000).log10().sub(5).max(0))))),
+    effect: () => Decimal.pow(player.endgame.celDimExpansion.celestialInfinities, 0.5).div(100).min(1).add(
+      DC.D4.times(DC.D1.sub(Decimal.pow(0.8, player.endgame.celDimExpansion.celestialInfinities.log10().sub(4).max(0))))),
     formatEffect: value => `${TimeSpan.fromHours(value).toStringShort()}`
   },
   celDimBoostBuff: {
